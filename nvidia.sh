35efc8f5f03ce66f4906ebce65b35ab3684b9608
Victor Fugazzotto <victorfuga@gmail.com>
2024-07-26 09:29:39 -0400

    Add Nvidia drivers

---
+++ nvidia.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $UID != 0 ]; then
+    echo "Script must be run as root"
+    exit 1
+fi
+
+# Install open kernel modules
+zypper -n in nvidia-open-driver-G06-signed-kmp-default kernel-firmware-nvidia-gspx-G06
+
+# Install display drivers
+zypper ar -p 99 https://download.nvidia.com/opensuse/tumbleweed/ NVIDIA
+zypper --gpg-auto-import-keys ref
+zypper -n in nvidia-video-G06 nvidia-gl-G06 nvidia-compute-G06
+
+# Additional utils
+zypper -n in nvidia-utils-G06 nvidia-compute-utils-G06
