2e8b613a42141e2c22f5bf63e6b79af3dfe7c602
vic485 <victorfuga@gmail.com>
2024-12-28 12:57:31 -0500

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -8,6 +8,9 @@ fi
 # Install open kernel modules
 zypper -n in nvidia-open-driver-G06-signed-kmp-default kernel-firmware-nvidia-gspx-G06
 
+# Make sure modules get loaded
+dracut -f
+
 # Install display drivers
 zypper ar -p 99 https://download.nvidia.com/opensuse/tumbleweed/ NVIDIA
 zypper --gpg-auto-import-keys ref
